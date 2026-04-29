be0030d927e58e0bfd6976ec6b68b7973f635e57
rubencrow32 <ruben_crow@hotmail.com>
2026-04-28 22:50:33 -0300

    agregar postgresql

--- catering_project/settings.py
+++ catering_project/settings.py
@@ -48,11 +48,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'catering_project.wsgi.application'
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
